4fc285023f710271779fa00654f4afe3c16e2f1b
Melissa Tejada <mtejada073@soyudemedellin.edu.co>
2022-08-17 23:31:25 -0500

    Primer Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+import requests
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
+    response = requests.get(url, {}, timeout=5)
+    return {"items": response.json() }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
