68aef8e4f30b093e4926300215cd66823247626e
Melissa Tejada <mtejada073@soyudemedellin.edu.co>
2022-08-18 20:15:41 -0500

    Agregar Logs

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from typing import Union
 from fastapi import FastAPI
 import requests
+from prometheus_fastapi_instrumentator import Instrumentator
+import logging
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+# get root logger
+logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
+                                      # This will get the root logger since no logger in the configuration has this name.
 
 
 @app.get("/")
